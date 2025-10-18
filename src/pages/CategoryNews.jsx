48ce906e018745d0d068931564bb02b9c21d00ab
sohidul8305 <si616132@gmail.com>
2025-10-18 19:45:44 +0600

    load data and used filter and usestate and use effect

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
     const {id} = useParams();
-    console.log(id);
+    const data = useLoaderData();
+    const [CategoryNews, setCategoryNews] = useState([]);
+    // console.log(id, data);
+    useEffect(()=>{
+        if(id == "0"){
+            setCategoryNews(data);
+            return;
+        }
+        else if(id == "1"){
+        const filterNews = data.filter((news) => news.others.is_today_pick == true
+    );
+  setCategoryNews(filterNews)
+        } else{
+        const filterNews = data.filter((news) => news.category_id == id);
+        console.log(filterNews);
+        setCategoryNews(filterNews);
+        }
+
+    },[data, id]);
     return (
         <div>
-            <h2>Categories News - {id}</h2>
+            <h2>Total {CategoryNews.length} Found</h2>
         </div>
     );
 };
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -4,32 +4,32 @@ import Home from "../pages/Home";
 import CategoryNews from "../pages/CategoryNews";
 
 const router = createBrowserRouter([
-{
+  {
     path: "/",
     element: <HomeLayout></HomeLayout>,
     children: [
-        {
-            path: "",
-            element: <Home></Home>
-        },
-        {
-            path: "/category/:id",
-            element: <CategoryNews></CategoryNews>
-        }
-    ]
-},
-{
+      {
+        path: "",
+        element: <Home></Home>,
+      },
+      {
+        path: "/category/:id",
+        element: <CategoryNews></CategoryNews>,
+        loader: () => fetch("/news.json"),
+      },
+    ],
+  },
+  {
     path: "/auth",
     element: <h2>Auththiication alyout</h2>,
-},
-{
+  },
+  {
     path: "/news",
     element: <h2>News Layout</h2>,
-},
-{
+  },
+  {
     path: "/*",
     element: <h2>Erroe 404</h2>,
-},
-
+  },
 ]);
 export default router;
